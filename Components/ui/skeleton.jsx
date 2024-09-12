9c0704c868e8da70622eb1c127ee1a4fe1a36fce
Muhammad Kamran <kamrankhanmashwani@gmail.com>
2024-09-12 12:58:50 +0500

    .

--- Components/ui/skeleton.jsx
+++ Components/ui/skeleton.jsx
@@ -1,14 +1,12 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Skeleton({
-  className,
-  ...props
-}) {
+function Skeleton({ className, ...props }) {
   return (
-    (<div
-      className={cn("animate-pulse rounded-md bg-zinc-100 dark:bg-zinc-800", className)}
-      {...props} />)
+    <div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props}
+    />
   );
 }
 
-export { Skeleton }
+export { Skeleton };
